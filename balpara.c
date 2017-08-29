1ccd676f6c10ba967147e487b8c017fcbd383810
heenakhan1908 <30309410+heenakhan1908@users.noreply.github.com>
2017-08-29 14:25:24 +0530

    first commit

--- balpara.c
+++ balpara.c
@@ -1,10 +1,15 @@
+/*
+*Description:balanced parathesis program
+*Author:khanheena
+*Roll no:16co04
+*/
 #include<stdio.h>
 #include<ctype.h> //for isalpha() function
 
 typedef struct balanced
 {
 char a[30];
- int top,i,c;
+ int top;
  }stack;
  
  void push(stack *s ,char c);
@@ -17,20 +22,19 @@ int isMatchingpair(char left,char right)
 {
  switch (left)
 {
-  case'(': if(right==')')
-   return 1;
-  else 
-   return 0;
-  break;
-  case'{': if(right=='}')
-   return 1;
-  else
-    return 0;
-  break;
-  case'[' : if(right==']')
-    return 1;
-  else
-    return 0;
+  case '(' : if(right==')')
+	   return 1;
+	   else 
+	   return 0;
+ 
+  case '{' : if(right=='}')
+	   return 1;
+	  else
+	    return 0;
+  case '[' : if(right==']')
+	    return 1;
+	  else
+	    return 0;
   default:
 		 return 0;
    
@@ -39,45 +43,37 @@ int isMatchingpair(char left,char right)
 
 void checkcountinuity(char exp[])
 {
-int i;
-char c;
-stack s;
-s.top==-1;
-for (i=0;exp[i]!='\0';i++)
+ int i;
+ char c;
+ stack s;
+ s.top==-1;
+ for (i=0;exp[i]!='\0';i++)
 {
- if (exp[i]=='{'||'('||'[')
- push(&s,exp[i]);
- else
- {
-	 if(s.top==-1)
-         printf("expression is invalid");
- else
-        return pop(&s);
-	 
- if (isMatchingpair(c,exp[i]))
-      continue;
- else
- {
-    printf("expression is invalid");
-    return;
-}
-}
-}
-if(s.top==-1)
-printf("expression is valid");
-else
-printf("expression is invalid");
+	 if (exp[i]=='{'||exp[i]=='('||exp[i]=='[')
+	 push(&s,exp[i]);
+	 else
+	 {
+		 if(s.top==-1)
+		 printf("expression is invalid");
+	 else
+		return pop(&s);
+
+	 if (isMatchingpair(c,exp[i]))
+	      continue;
+	 else
+	 {
+	    printf("expression is invalid");
+	    return;
+	}
+	}
+	}
+	if(s.top==-1)
+	printf("expression is valid");
+	else
+	printf("expression is invalid");
 }
  
- 
- 
-
-
-
-
-
-
-void push(stack *s,char opr)
+ void push(stack *s,char opr)
 {
 	if(s->top>29)
 	{
