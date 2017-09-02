6973c3e4578ea538ea9af70029baeb93dc34d0e5
heenakhan1908 <30309410+heenakhan1908@users.noreply.github.com>
2017-09-02 21:32:14 +0530

    Update cqueue.c

--- cqueue.c
+++ cqueue.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define MAX_SIZE 7
-#includemath.h
+
 int queue[MAX_SIZE],front=-1,rear=-1;
 
 int isFull()
@@ -12,53 +12,55 @@ int isFull()
 
 int isEmpty(){
 
-	return front==-1 && rear==-1;
+	return front==-1&&rear==-1;
 	
 }
 
-int peek(){
-
-	return queue[front];
+void display(){
+	int i=front;
+	if(!isEmpty()){
+		while(i!=rear){
+			printf("%d\t",queue[i]);
+			i=(i+1)%MAX_SIZE;
+		}
+		printf("%d\t",queue[i]);
+	}
+	else{
+		printf("\nQueue is Empty.");
+	}	
+			
 }
 
-
-
-void insertqueue(int d)
+void insertqueue(int e)
 {
 if(!isFull())
-{
-rear=(rear+1)%(MAX-SIZE);
-queue
-if(isEmpty())
-{
-	front++;
-}
-rear=(rear++)%MAX_SIZE;
-queue[rear]=d;
-printf("%d sucesfully inserted",d);
-}
-else
-printf("queue is full");
+	{
+		rear=(rear+1)%(MAX_SIZE);
+		queue[rear]=e;
+	        if(front==-1)
+			front++;
+		printf("\nElement %d is successfully Inserted",e);
+		printf("\n%d-%d",front,rear);
+	}
+	else
+		printf("\nQueue is Full Element cannot be inserted.");	
 }
 
-
-
-void delete()
+void deletequeue()
 {
-if (!isEmpty())
+	intd;
+	if (!isEmpty())
 {
-int d;
-d=queue[front];
-{
-if(front==rear)
-front=rear=-1;
-else
-front=(front++)%MAX_SIZE;
-}
-printf("the element deleted is %d",d);
+	d=queue[front];
+	if(front==rear)
+		front=rear=-1;
+	else
+		front=(front++)%(MAX_SIZE);
+	printf("the element deleted is %d",d);
+	printf("\n%d-%d",front,rear);		
 }
 else
-printf("queue is empty");
+	printf("queue is empty");
 }
 
 int main(){
@@ -71,16 +73,17 @@ int main(){
 		scanf("%d",&choice);	
 		switch(choice){
 			case 1:
-				e=peek();
-				printf("\nElement at the front of queue is : %d",e);
+				case 1:
+				printf("\nElements of queue are");
+				display();
 				break;
 			case 2:
 				printf("\nEnter the elment to be inserted: ");
 				scanf("%d",&e); 
-			  insertqueue(e);
+			        insertqueue(e);
 				break;
 			case 3:
-			delete();
+				deletequeue();
 				break;
 			case 4:
 				exit(0);
