3cc0af87b310e4e7937863ea0d4346b6ccae48e6
heenakhan1908 <30309410+heenakhan1908@users.noreply.github.com>
2017-09-17 23:27:10 +0530

    Update cqueue.c

--- cqueue.c
+++ cqueue.c
@@ -98,3 +98,101 @@ int main(){
 	}while(1);
 	return 0;
 }
+
+/********OUTPUT********
+
+Enter.....
+1.display
+2.insert
+3.delete
+4.Exit
+Enter your choice: 3
+queue is empty
+
+
+Enter.....
+1.display
+2.insert
+3.delete
+4.Exit
+Enter your choice: 2
+
+Enter the element to be inserted:
+1
+
+Element 1 is successfully Inserted
+0-0
+
+
+Enter.....
+1.display
+2.insert
+3.delete
+4.Exit
+Enter your choice: 2
+
+Enter the element to be inserted:
+2
+
+Element 2 is successfully Inserted
+0-1
+
+
+Enter.....
+1.display
+2.insert
+3.delete
+4.Exit
+Enter your choice: 2
+
+Enter the element to be inserted:
+3
+
+Element 3 is successfully Inserted
+0-2
+
+
+Enter.....
+1.display
+2.insert
+3.delete
+4.Exit
+Enter your choice: 2
+
+Enter the element to be inserted:
+4
+
+Element 4 is successfully Inserted
+0-3
+
+
+Enter.....
+1.display
+2.insert
+3.delete
+4.Exit
+Enter your choice: 3
+
+the element deleted is 1
+1-3
+
+
+Enter.....
+1.display
+2.insert
+3.delete
+4.Exit
+Enter your choice: 1
+Elements of queue are
+2       3       4
+
+Enter.....
+1.display
+2.insert
+3.delete
+4.Exit
+Enter your choice: 4
+
+Process returned 0 (0x0)   execution time : 47.363 s
+Press any key to continue.
+*/
